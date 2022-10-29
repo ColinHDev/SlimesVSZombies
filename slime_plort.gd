e2222bf7385c1f1b6c76dfcaca132dad9d3f6de2
Kfalke <116970424+Kfalke@users.noreply.github.com>
2022-10-29 22:09:59 +0200

    Revert "ajhvsc"
    This reverts commit d8107b98bd310e35c5b11e22dfb8777bcc6962e3.

--- slime_plort.gd
+++ slime_plort.gd
@@ -1,13 +1,7 @@
 extends Label
 
-export var plort_count = 0
-
 func _ready():
 	text = "0"
 	
 func _process(delta):
-	text = String(plort_count)
-
-
-func _on_Button_pressed():
-	plort_count = plort_count + 1
+	text = String(text.to_int() + 1)
