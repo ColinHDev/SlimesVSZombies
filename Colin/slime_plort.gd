e4be8c838b2fab951eae49262755e1fe81b92ebd
ColinHDev <colinheidfeld@gmail.com>
2022-10-30 12:45:17 +0100

    Removed slime button

--- Colin/slime_plort.gd
+++ Colin/slime_plort.gd
@@ -6,7 +6,3 @@ func _ready():
 func _process(delta):
 	text = String(text.to_int() + 1)
 	text = String(Game.plort_count)
-	
-
-func _on_plort_button_pressed():
-	Game.plort_count = Game.plort_count + 1
