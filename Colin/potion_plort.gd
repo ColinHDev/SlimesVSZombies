95ebd3e3e6dc53ac77d10f15826631b5c410d4a4
ColinHDev <colinheidfeld@gmail.com>
2022-10-30 12:42:37 +0100

    removed forgotten debug statement

--- Colin/potion_plort.gd
+++ Colin/potion_plort.gd
@@ -8,7 +8,6 @@ func _process(delta):
 	text = String(Game.potion_count)
 
 func _on_potion_button_pressed():
-	print(Game.plort_count)
 	if (Game.plort_count >= 5):
 		Game.potion_count = Game.potion_count + 1
 		Game.plort_count -= 5
