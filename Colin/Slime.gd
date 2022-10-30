4608628dd1be63f24cfbb0971caa3548353e9091
ColinHDev <colinheidfeld@gmail.com>
2022-10-30 19:34:18 +0100

    removed some unnecessary stuff

--- Colin/Slime.gd
+++ Colin/Slime.gd
@@ -11,7 +11,7 @@ export var size = 2
 func _ready():
 	$AnimatedSprite.scale.x = size * 4
 	$AnimatedSprite.scale.y = size * 4
-	
+
 func _process(delta) -> void:
 	for i in range(0, $AnimatedSprite.frames.get_frame_count($AnimatedSprite.animation)):
 		var collisionFrame: CollisionPolygon2D = get_node("CollisionFrame" + str(i))
@@ -21,7 +21,6 @@ func _process(delta) -> void:
 			collisionFrame.scale.y = $AnimatedSprite.scale.y
 		else:
 			collisionFrame.disabled = true
- 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -32,9 +31,6 @@ func _physics_process(delta):
 		queue_free()
 		return
 	position.y += 1
-	"""var collision: KinematicCollision2D = move_and_collide(Vector2(0.0, 1.0), true, true, true)
-	if collision == null || collision.collider_id < get_instance_id():
-		position.y += 1"""
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
