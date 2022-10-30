e066b2f17a6e042c2494d17ab13d4c7c3a71a73c
ColinHDev <colinheidfeld@gmail.com>
2022-10-30 16:56:06 +0100

    ...

--- Colin/Game.gd
+++ Colin/Game.gd
@@ -7,6 +7,9 @@ extends Node2D
 
 onready var Slime = preload("res://Colin/Slime.tscn")
 
+func _ready():
+	spawnSlimes(10, 2, 10)
+
 var plort_count = 0
 var potion_count = 0
 var heal_count = 0
@@ -14,6 +17,12 @@ var player_lives = 3
 
 var minX = 400
 var maxX = 1200
+
+func spawnSlimes(number: int, maxSize: int, time: int) -> bool:
+	if (spawner != null):
+		return false;
+	spawner = load("res://Colin/Spawner.gd").new(number, maxSize, time)
+	return true
 
 # Spawns a slime with the given size at the given coordinates
 func spawnSlime(size: int, x: float, y: float) -> void:
@@ -23,8 +32,12 @@ func spawnSlime(size: int, x: float, y: float) -> void:
 	slimeInstance.position.y = y
 	self.add_child(slimeInstance)
 
-var rng = RandomNumberGenerator.new()
+var spawner = null
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if spawner != null:
+		var toSpawn: int = spawner.getAmountToSpawn()
+		while toSpawn > 0:
+			spawnSlime(spawner.getRandomSize(), spawner.getRandomX(), 100.0)
+			toSpawn -= 1
---
+++ Colin/Spawner.gd
@@ -0,0 +1,52 @@
+extends Node
+
+var number: int
+var maxSize: int
+var time: int
+var startTime: int
+var lastSpawn: int = 0
+var rng: RandomNumberGenerator
+
+
+func _init(numberOfSlimes: int, slimeMaxSize: int, timeToSpawn: int):
+	number = numberOfSlimes
+	maxSize = slimeMaxSize
+	time = timeToSpawn
+	startTime = OS.get_system_time_secs()
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
+
+func hasFinished() -> bool:
+	return OS.get_system_time_secs() - startTime > time
+
+func getAmountToSpawn() -> int:
+	if hasFinished():
+		return 0
+	var secondsInCycle = OS.get_system_time_secs() - startTime
+	if lastSpawn == secondsInCycle:
+		return 0
+	lastSpawn = secondsInCycle
+	
+	var x: float = (float(number) / float(time))
+	return int(x)
+	"""var mean: float = float(number) / 2.0
+	var deviation: float = mean / 3.0
+	var gaussian: float = gaussian(mean, deviation, x)
+	
+	for i in range(1, 32):
+		gaussian += gaussian(mean, deviation, (float(number) / float(time)) * (float(secondsInCycle) + pow(10, - i)))
+	
+	print(str(secondsInCycle) + " - " + str(float(secondsInCycle) + pow(10, - 32)))
+	print(gaussian)
+	print(gaussian * number)
+	print(int(round(gaussian * number)))
+	return int(round(gaussian * number));"""
+	
+func gaussian(mean: float, deviation: float, x: float) -> float:
+	return (1 / (deviation * sqrt(2 * PI))) * pow(exp(1), -0.5 * pow((x - mean) / deviation, 2))
+
+func getRandomSize() -> int:
+	return rng.randi_range(1, maxSize)
+
+func getRandomX() -> int:
+	return rng.randi_range(Game.minX, Game.maxX)
