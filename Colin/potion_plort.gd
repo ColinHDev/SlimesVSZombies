0d86a2ca6d37ca4e104aa90aea2fd1d003ff6c00
ColinHDev <colinheidfeld@gmail.com>
2022-10-30 11:27:24 +0100

    fixed potion buying

--- Colin/potion_plort.gd
+++ Colin/potion_plort.gd
@@ -1,8 +1,14 @@
 extends Label
 
+func _ready():
+	text = "0"
+	
+func _process(delta):
+	text = String(text.to_int() + 1)
+	text = String(Game.potion_count)
+
 func _on_potion_button_pressed():
 	print(Game.plort_count)
 	if (Game.plort_count >= 5):
 		Game.potion_count = Game.potion_count + 1
 		Game.plort_count -= 5
-		Game.get_node("player_panel/potion_subpanel/potion1").text = String(Game.potion_count)
