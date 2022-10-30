86004c673ec87f43da87ad90271b620726242538
ColinHDev <colinheidfeld@gmail.com>
2022-10-30 20:19:16 +0100

    reorganized files

--- ui.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-export var potion_count = 0
-
-func _on_potion_button_pressed():
-	if (get_node("player_panel/ressource_subpanel/slime_plort").plort_count >= 5):
-		potion_count = potion_count + 1
-		get_node("player_panel/ressource_subpanel/slime_plort").plort_count -= 5
-		get_node("player_panel/potion_subpanel/potion1").text = String(potion_count)
